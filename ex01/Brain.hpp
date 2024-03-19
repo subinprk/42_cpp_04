ed9fac981995d5df554d0989f59e612dfe800a9c
subinprk <subinnnp@gmail.com>
2024-03-19 02:39:21 +0100

    realized I didn't write WrongAnimal codes

--- ex01/Brain.hpp
+++ ex01/Brain.hpp
@@ -6,15 +6,20 @@
 /*   By: siun <siun@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 02:30:50 by siun              #+#    #+#             */
-/*   Updated: 2024/03/19 02:33:49 by siun             ###   ########.fr       */
+/*   Updated: 2024/03/19 02:38:16 by siun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BRAIN_HPP
 #define BRAIN_HPP
+#include <string>
+#include <iostream>
 
-class BrainL{
-
-}
+class Brain{
+	private:
+		std::string ideas[100];
+	// public:
+		
+};
 
 #endif
