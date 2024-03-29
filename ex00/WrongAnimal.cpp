0f8d5456e129b40da1df3d6c9ec39eb53973d6c9
subinprk <subpark@student.42berlin.de>
2024-03-29 15:05:25 +0100

    adding WrongAnimal in ex00

--- ex00/WrongAnimal.cpp
+++ ex00/WrongAnimal.cpp
@@ -6,9 +6,41 @@
 /*   By: subpark <subpark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 21:49:06 by subpark           #+#    #+#             */
-/*   Updated: 2024/03/23 21:53:52 by subpark          ###   ########.fr       */
+/*   Updated: 2024/03/29 15:05:09 by subpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "WrongAnimal.hpp"
 
+WrongAnimal::WrongAnimal()
+{
+    std::cout << "WrongAnimal Default Constructor called\n";
+    _type = "WrongAnimal";
+}
+
+WrongAnimal::WrongAnimal(const WrongAnimal &copy)
+{
+    std::cout << "copy in WrongAnimal class called\n";
+    *this = copy;
+}
+
+WrongAnimal::~WrongAnimal()
+{
+    std::cout << "Default WrongAnimal Destructor called\n";
+}
+
+WrongAnimal &WrongAnimal::operator=(const WrongAnimal &copy)
+{
+    _type = copy._type;
+    return (*this);
+}
+
+void    WrongAnimal::makeSound() const
+{
+    std::cout << "WrongAnimal making Sound\n";
+}
+
+std::string WrongAnimal::getType() const
+{
+    return (_type);
+}
--- ex00/WrongAnimal.hpp
+++ ex00/WrongAnimal.hpp
@@ -6,23 +6,28 @@
 /*   By: subpark <subpark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 21:49:12 by subpark           #+#    #+#             */
-/*   Updated: 2024/03/23 21:46:38 by subpark          ###   ########.fr       */
+/*   Updated: 2024/03/29 14:12:49 by subpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WRONGANIMAL_HPP
 #define WRONGANIMALL_HPP
+#include "string"
+#include "iostream"
 
 class WrongAnimal{
     private:
 
     protected:
-
+        std::string _type;
     public:
         WrongAnimal();
+    //    WrongAnimal(const std::string type);
         WrongAnimal(const WrongAnimal &copy);
-        virtual ~WrongAnimal();
-        Animal
+        ~WrongAnimal();
+        WrongAnimal &operator=(const WrongAnimal &copy);
+        void    makeSound() const;
+        std::string getType() const;
 };
 
 #endif
